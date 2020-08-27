54bfa30bb5979613ebbfa7e54cb0eacab74c2ea5
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-08-27 20:53:19 -0300

    Concluido o desafio  2/2

--- Desafios/01-intrudcao-a-programacao-web/desafio-1-2/vetores-e-objetos.js
+++ Desafios/01-intrudcao-a-programacao-web/desafio-1-2/vetores-e-objetos.js
@@ -1,7 +1,20 @@
-const objeto = {
-  propriedade: [
-    { nome: 'C++', especialidade: 'Desktop' },
-    { nome: 'Python', especialidade: 'Data Science' },
-    { nome: 'JavaScript', especialidade: 'Web/Mobile' },
+const programador = {
+  nome: [
+    'Johnny',
+    'Tatiana',
+    'Larissa',
+    'Adriano',
+    'Carlos',
+    'André',
+    'Felipe',
+  ],
+  idade: 26,
+  tecnologias: [
+    { nome: 'javascript', especialidade: 'Web/Mobile' },
+    { nome: 'HTML/CSS', especialidade: 'Web' },
   ],
 };
+
+console.log(
+  `O usuário ${programador.nome[2]} tem ${programador.idade} anos e usa a técnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`
+);
