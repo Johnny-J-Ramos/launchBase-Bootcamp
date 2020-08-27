60a4457592b22e30a3a5d90a64e25cedcd8f482c
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-08-26 21:53:29 -0300

    concluido o primeiro desafio

--- Desafios/01-intrudcao-a-programacao-web/desafio-1-1/js/calculo-de-imc.js
+++
@@ -1,4 +0,0 @@
-const nome = 'johnny';
-const idade = 26;
-
-console.log(nome);
---
+++ Desafios/01-intrudcao-a-programacao-web/desafio-1-2/construcao-e-impressao-de-objetos.js
@@ -0,0 +1,15 @@
+const empresa = [
+  {
+    nome: 'Rockeseat',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereco: {
+      Rua: 'Rua Guilherme Gembala',
+      Numero: 260,
+    },
+  },
+];
+
+console.log(
+  `A empresa ${empresa[0].nome} está localizada em ${empresa[4].Rua}`
+);
