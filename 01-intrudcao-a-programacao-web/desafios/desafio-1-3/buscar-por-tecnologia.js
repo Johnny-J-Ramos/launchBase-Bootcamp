4751c0834af97dc87a19aacc3dcaf32cd211b450
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-08-29 11:02:22 -0300

    desafio 3 iniciado

---
+++ 01-intrudcao-a-programacao-web/desafios/desafio-1-3/buscar-por-tecnologia.js
@@ -0,0 +1,28 @@
+/*=========================================
+DESAFIO 1.3
+
+    Criar uma função que receba os dados de um objeto de usuário e torna
+    SE o usuário trabalho com CSS ou não. Essa função deve retornar um boolean true/false.
+
+
+==========================================*/
+
+const usuarios = [
+  {
+    nome: 'Johnny',
+    tecnologias: ['HTML', 'CSS'],
+  },
+  {
+    nome: 'larissa',
+    tecnologias: ['Javascript', 'CSS'],
+  },
+  {
+    nome: 'Luana',
+    tecnologias: ['Docker', 'Node.js'],
+  },
+];
+
+function checaUsuariousaCSS() {
+  if usuario = false,
+  for (let )
+}
---
+++ 01-intrudcao-a-programacao-web/desafios/desafio-1-3/usuarios-e-tecnologias.js
@@ -0,0 +1,24 @@
+// Crie um programa que armazena uma array de usuário (objetos),
+// cada usuário tem um nome e suas tecnologias (novo array).
+
+const usuarios = [
+  {
+    nome: 'Johnny',
+    tecnologias: ['HTML', 'CSS'],
+  },
+  {
+    nome: 'larissa',
+    tecnologias: ['Javascript', 'CSS'],
+  },
+  {
+    nome: 'Luana',
+    tecnologias: ['Docker', 'Node.js'],
+  },
+];
+
+/*for (let usuario of usuarios) {
+  console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`);
+}*/
+for (let usuario of usuarios) {
+  console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`);
+}
