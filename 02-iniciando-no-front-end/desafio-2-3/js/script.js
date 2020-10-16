3608419d4967731023ca63d84f92932056d1f310
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-10-16 20:08:28 -0300

    iniciando o desafio 2-3 do frontend

---
+++ 02-iniciando-no-front-end/desafio-2-3/js/script.js
@@ -0,0 +1,10 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+  card.addEventListener('click', function () {
+    modalOverlay.classList.add('active');
+  });
+}
+
+modalOverlay.querySelector('.material-icons').addEventListener;
