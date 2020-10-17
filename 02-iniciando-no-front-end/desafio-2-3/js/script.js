d5e9afb4ecc2d16679922f03794222449fbb0b83
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-10-17 18:34:39 -0300

    finalizando o desafio 2-3

--- 02-iniciando-no-front-end/desafio-2-3/js/script.js
+++ 02-iniciando-no-front-end/desafio-2-3/js/script.js
@@ -1,10 +1,15 @@
-const modalOverlay = document.querySelector('.modal-overlay');
+const modalOverley = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
   card.addEventListener('click', function () {
-    modalOverlay.classList.add('active');
+    modalOverley.classList.add('active');
   });
 }
 
-modalOverlay.querySelector('.material-icons').addEventListener;
+const closeModal = document
+  .querySelector('.close-modal')
+  .addEventListener('click', function () {
+    modalOverley.classList.remove('active');
+    modalOverley.querySelector('iframe').src = '';
+  });
