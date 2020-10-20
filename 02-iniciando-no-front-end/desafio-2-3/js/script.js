908be151a3ba10a0d7a285f89c40a87deca54555
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-10-20 20:20:56 -0300

    concluido o desafio 2-3

--- 02-iniciando-no-front-end/desafio-2-3/js/script.js
+++ 02-iniciando-no-front-end/desafio-2-3/js/script.js
@@ -1,19 +1,32 @@
 const modalOverley = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
 const cards = document.querySelectorAll('.card');
+let modalMaximized = false;
 
 for (let card of cards) {
-  card.addEventListener('click', function () {
+  card.addEventListener('click', () => {
     modalOverley.classList.add('active');
+    modalOverley.querySelector('iframe').src =
+      'https://blog.rocketseat.com.br/upload-de-imagens-no-front-end-com-react-js-e-context-api-3/';
   });
 }
 
-const closeModal = document
-  .querySelector('.close-modal')
-  .addEventListener('click', function () {
-    modalOverley.classList.remove('active');
-    modalOverley.querySelector('iframe').src = '';
-  });
+document.querySelector('.close-modal').addEventListener('click', () => {
+  modalOverley.classList.remove('active');
+  modalOverley.querySelector('iframe').src = '';
+  modal.style.height = '80%';
+  modal.style.whith = '80%';
+  modalMaximized = false;
+});
 
-const MaxiModal = document.querySelector('#mix').addEventListener('click', function(){
-  mod
-})
+document.querySelector('.maximize-modal').addEventListener('click', () => {
+  if (modalMaximized === false) {
+    modal.style.height = '100%'; //altura
+    modal.style.width = '100%';
+    modalMaximized = true;
+  } else {
+    modal.style.height = '80%';
+    modal.style.width = '80%';
+    modalMaximized = false;
+  }
+});
