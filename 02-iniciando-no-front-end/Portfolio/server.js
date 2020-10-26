aa47d875940203c365dd17f4c84c0dadd4fbeb1a
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-10-26 20:01:57 -0300

    atualizando os arquivos

---
+++ 02-iniciando-no-front-end/Portfolio/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get('/', function (req, res) {
+  return res.send('Hello, how are you? and you?');
+});
+
+server.listen(5000, function () {
+  console.log('not dfldlfdkjkjkjl');
+});
